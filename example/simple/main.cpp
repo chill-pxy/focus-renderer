fa2cb4c9aa34a56cc307b0bc93bf3f700b4973c1
ppy <1321337557@qq.com>
2024-06-27 11:13:03 +0800

    更新vulkan rhi

--- example/simple/main.cpp
+++ example/simple/main.cpp
@@ -1,13 +1,37 @@
-import window_context;
+//import window_context;
 import vulkan_rhi;
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <limits>
+#include <optional>
+#include <set>
 
 int main()
 {
-	auto window = new FOCUS::WindowContext();
+	/*auto window = new FOCUS::WindowContext();
 	window->init();
 
 	auto vulkan = new FOCUS::VulkanRHI();
 	vulkan->init(window->getWindowInstance());
 
-	return 0;
+	return 0;*/
+
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }

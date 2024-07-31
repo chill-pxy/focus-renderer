5a7734bbe8d82470f9ed840363e8ca889c4a35e8
ppy <1321337557@qq.com>
2024-07-31 17:12:58 +0800

    vulkan instance 实现

--- Engine/Platform/Interface/Vulkan/Instance.cpp
+++ Engine/Platform/Interface/Vulkan/Instance.cpp
@@ -1,22 +1,109 @@
+#include <stdexcept>
+#include <vector>
+
 #include <volk.h>
+#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include "../Instance.h"
 #include "../InterfaceType.h"
 
-namespace RHI
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+namespace FOCUS
 {
+    //-----------------------------//
+    //-----------------------------//
+    //------ public function ------//
+    //-----------------------------//
+    //-----------------------------//
+    bool checkValidationLayerSupport() 
+    {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        for (const char* layerName : validationLayers) {
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayers) {
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if (!layerFound) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    std::vector<const char*> getRequiredExtensions() 
+    {
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        if (enableValidationLayers) {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        return extensions;
+    }
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+        VkDebugUtilsMessageTypeFlagsEXT messageType, 
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, 
+        void* pUserData) 
+    {
+        //std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+        return VK_FALSE;
+    };
+
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) 
+    {
+        createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+    }
+
+    //--------------------------------//
+    //--------------------------------//
+    //------- platform function ------//
+    //--------------------------------//
+    //--------------------------------//
 	namespace Platform
 	{
 		void Instance::createInstance(Instance& instance)
 		{
-            vkInstance = new VulkanInstance();
-            *instance = vkInstance;
+            instance = static_cast<VulkanInstance>(instance);
 
             //初始化volk
 			volkInitialize();
 
             //初始化vulkan instance
-            if (enableValidationLayers && !checkValidationLayerSupport()) {
+            if (enableValidationLayers && !checkValidationLayerSupport()) 
+            {
                 throw std::runtime_error("validation layers requested, but not available!");
             }
 
